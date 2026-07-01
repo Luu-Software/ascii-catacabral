0b0a887fb19a3d907f7970059d3a19e7cf9ad332
CatalinaCabral <catalinaisabelcabral@gmail.com>
2026-07-01 15:21:56 -0300

    base terminado

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,14 @@ import { preguntar } from './lib/consola.ts';
 function repetirLetra(letra: string, cantidad: number): string {
  let resultado="";
  let contador= 0;
-
-
+ while(contador < cantidad){
+    resultado=resultado + letra;
+    contador= contador + 1;
+   }
+   return resultado;
+}
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
-
- while(contador < cantidad){
-  resultado=resultado + letra;
-  contador= contador + 1;
- }
- return resultado;
 
  let fila: string = repetirLetra(caracter, tamaño)
 
@@ -22,4 +20,3 @@ let tamaño: number = Number(preguntar('¿De qué tamaño? '));
   filaActual= filaActual + 1;
  }
 
-}
